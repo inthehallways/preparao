ad0c5d093fa4b4baf2fa526e80ef5529d1583962
Celesse Aisle Nacpil <notaisle@gmail.com>
2025-12-03 02:09:53 +0800

    Add website header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home'
+import Cart from './pages/cart/Cart'
+import PlaceOrder from './pages/placeorder/PlaceOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/placeorder' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2> Order your favorite food here </h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
