4bfd144f1d13f3ed8af53db911c41134efd9d656
Celesse Aisle Nacpil <notaisle@gmail.com>
2025-12-04 02:46:46 +0800

    Setup initial Explore Menu list

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/header/Header'
+import ExploreMenu from '../../components/exploremenu/ExploreMenu'
 
 const Home = () => {
+
+    const [category, setCategory] = useState("All");
+
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   )
 }
