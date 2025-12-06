364e812eb5b0f36175f043bf86a65437b72dd643
Celesse Aisle Nacpil <notaisle@gmail.com>
2025-12-07 01:00:40 +0800

    Finish Login/Signup Modal, Cart and Place Order page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import React from "react"
+import React, { useState } from 'react';
 import Navbar from "./components/navbar/Navbar"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/home/Home"
 import Cart from "./pages/cart/Cart"
 import PlaceOrder from "./pages/placeorder/PlaceOrder"
+import LoginPopup from "./components/loginpopup/LoginPopup"
 import Footer from "./components/footer/footer"
 
+
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
