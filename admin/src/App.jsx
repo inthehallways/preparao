cffb8422e97fb14f2d3af0a0595b4dcb993b2910
Celesse Aisle Nacpil <notaisle@gmail.com>
2025-12-08 01:15:06 +0800

    Setup Admin Panel folder

---
+++ admin/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Sidebar from './components/sidebar/Sidebar'
+import { Routes, Route} from 'react-router-dom'
+import Add from './pages/add/Add'
+import List from './pages/list/List'
+import Orders from './pages/orders/Orders'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <hr/>
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/list" element={<List/>}/>
+          <Route path="/orders" element={<Orders/>}/>
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
