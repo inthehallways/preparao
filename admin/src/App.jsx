3c75a4597245592c75aa38efdec53674433f7047
Celesse Aisle Nacpil <notaisle@gmail.com>
2025-12-08 18:24:38 +0800

    Finish Add.jsx and its backend setup

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,6 +5,8 @@ import { Routes, Route} from 'react-router-dom'
 import Add from './pages/add/Add'
 import List from './pages/list/List'
 import Orders from './pages/orders/Orders'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
