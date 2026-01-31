f6d65f6a0517fe14fbe6aef84a1ea127fee1a275
Celesse Aisle Nacpil <notaisle@gmail.com>
2026-02-01 01:37:14 +0800

    fix: secure database credentials using env variables
    cnong tnga ang naglagay ng credentials sa db (me) 😭✋
    nyahahha woops i guess atlis ok n!! 🙂‍↕️

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    (await mongoose.connect('mongodb+srv://notaisle:JugheadgabbaE@cluster0.fhal4k9.mongodb.net/preparao').then(() => console.log("MongoDB connected")));
+    (await mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected")));
 }
